c67d3aa2df13014c471aa32d5d052c9b9ff0e96a
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-07-03 14:34:45 +0530

    modified to apply more validation

--- main/controller/user.controller.js
+++ main/controller/user.controller.js
@@ -6,29 +6,36 @@ exports.userRegister = (req, res) => {
     try {
         var response = {};
 
-        req.checkBody("firstName").exists();
-        req.checkBody("lastName").exists();
-        req.checkBody("emailID").exists().isEmail();
-        req.checkBody("password").exists();
+        req.checkBody("firstName")
+            .isAlpha().withMessage('first name is not in proper formate')
+            .isLength({ min: 4 }).withMessage('first name should have min 4 characters')
+            .exists();
+        req.checkBody("lastName")
+            .isAlpha().withMessage('last name is not in proper formate')
+            .isLength({ min: 4 }).withMessage('last name should have min 4 characters')
+            .exists();
+        req.checkBody("emailID")
+            .exists()
+            .isEmail();
+        req.checkBody("password")
+            .matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#$^+=!*()@%&]).{8,}$/)
+            .withMessage('must be at least 8 chars long and must and min one lower and uper case chars aslo only one special chars ')
+            .exists();
 
         const error = req.validationErrors();
-
         if (error) {
             response.success = false;
             response.message = "enter valid details";
             response.error = error;
             return res.status(500).send(response);
         } else {
-
             userDetails = {
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 emailID: req.body.emailID,
                 password: bcrypt.hashSync(req.body.password, 10),
             };
-
             USER_SERVICE.registerUser(userDetails, (err, data) => {
-
                 if (err) {
                     response.success = false;
                     response.message = "There was a problem registering the user";
